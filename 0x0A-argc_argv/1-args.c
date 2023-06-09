cb73f5cff509cddc32cb357076e33a53b5737932
am1996 <ahmedmagdy92@yahoo.com>
2023-06-09 14:15:44 -0700

    go

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,14 +7,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int i;
-
-	if (argc >= 1)
-	{
-		for (i = 0; i < argc; i++)
-		{
-			printf("%s\n", argv[i]);
-		}
-	}
+	argv = argv;
+	printf("%d\n", argc - 1);
 	return (0);
 }
